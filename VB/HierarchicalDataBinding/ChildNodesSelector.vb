ada8dd2707523a9cb34f6684147f3b9b2d3446a3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ VB/HierarchicalDataBinding/ChildNodesSelector.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports DevExpress.Xpf.Grid
+Imports System.Collections
+
+Namespace HierarchicalDataBinding
+	Public Class CustomChildrenSelector
+		Implements IChildNodesSelector
+        Public Function SelectChildren(ByVal item As Object) As IEnumerable Implements IChildNodesSelector.SelectChildren
+            If TypeOf item Is Task Then
+                Return Nothing
+            ElseIf TypeOf item Is ProjectStage Then
+                Return (TryCast(item, ProjectStage)).Tasks
+            ElseIf TypeOf item Is ProjectObject Then
+                Return (TryCast(item, ProjectObject)).Stages
+            End If
+            Return Nothing
+        End Function
+	End Class
+End Namespace
